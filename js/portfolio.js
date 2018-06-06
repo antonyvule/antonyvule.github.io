8bbebb2e4a0d987efdb5ac4d4d4ab7a441076f31
Antony Vu Le <antonyvule@gmail.com>
2018-06-06 14:26:00 +1000

    Updated to the latest version

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,10 +1,24 @@
 (function($) {
 
+	$window = $(window);
+
 	$('.page-scroll a').on('click', function(e) {
 		e.preventDefault();
 		$('html, body').stop().animate({
 			scrollTop: $($(this).attr('href')).offset().top - 56
 		}, 1286, 'easeInOutExpo');
 	});
+
+    $window.scroll(function(){
+        $('.fadein').each(function(){
+        	var $this = $(this);
+            var bottom_of_object = $this.offset().top + $this.outerHeight();
+            var bottom_of_window = $window.scrollTop() + $window.height();
+            
+            if(bottom_of_window > bottom_of_object){
+                $this.animate({'opacity': '1'}, 786);
+            }
+        });
+    });
 
 })(jQuery);
